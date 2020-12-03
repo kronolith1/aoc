32fdc326ee4181e1b5c1d3cc4ca66925ea58862c
l.both@youngcapital.nl <l.both@youngcapital.nl>
2020-12-03 14:25:20 +0100

    Finished day 1

---
+++ Day 1: Report Repair/part2.py
@@ -0,0 +1,14 @@
+with open('input.txt') as input_file:
+    input_array = input_file.readlines()
+
+for x in input_array:
+    x = int(x)
+    for y in input_array:
+        y = int(y)
+        for z in input_array:
+            z = int(z)
+            if x + y + z == 2020:
+                print(f"X is: {x}")
+                print(f"Y is: {y}")
+                print(f"Z is: {z}")
+                print(f"Answer is: {x * y * z}")
